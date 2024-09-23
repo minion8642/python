563400753028243e2c64deaae4cde5cf86ddc09b
minion8642 <alina.pronina070208@gmail.com>
2024-09-23 16:46:55 +0300

    Create задание 1, 2, 3 Пронина.py

---
+++ задание 1, 2, 3 Пронина.py
@@ -0,0 +1,32 @@
+#Задание 1
+interget_var = 5
+float_var = 2.5
+string_var = "Leon"
+list_var = [0, 4, 0, 7]
+tuple_var = (125, 115, 995)
+dict_var = {1: "leo1", 2: "leo2"}
+set_var = {5, 6, 7}
+bool_var = True
+print(interget_var)
+print(float_var)
+print(string_var)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(set_var)
+print(bool_var)
+#Задание 2
+name = 'Alina'
+age = '16'
+average_grade = 4.89
+favorite_movies = [ 'Человек паук', 'Москва слезам не верит', 'Мстители']
+personal_info = {'surname': 'Pronina', 'scholl number': '1', 'average_grade': '4.3'}
+skills = {'Play the piano'}
+visited_countries = {'Turkey', 'Ukraine'}
+has_driver_license = False
+#Задание 3
+CPU = 'Intel core i7-12700KF'
+volume_of_RAM_in_GB = 32
+average_internet_speed_in_megabytes_per_second = 65.89
+parameters = [CPU, volume_of_RAM_in_GB, average_internet_speed_in_megabytes_per_second]
+print(parameters)
